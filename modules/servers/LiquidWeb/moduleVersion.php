da35e7abf0a3829dddf1e7136cb97694e9b643d6
Santhosh Suvarna <santhosh.suvarna@glowtouch.com>
2017-01-23 20:19:27 +0530

    version changed to 1.2.3

--- modules/servers/LiquidWeb/moduleVersion.php
+++ modules/servers/LiquidWeb/moduleVersion.php
@@ -5,6 +5,6 @@
  * Below you can find current version & revision of this module
  */
 
-$moduleVersion = '1.2.2';
+$moduleVersion = '1.2.3';
 $moduleRevision = '531890abfda847fc9bcd944a04aa8e24c22525d1';
 $moduleWikiUrl = 'http://www.docs.liquidweb.com/';
